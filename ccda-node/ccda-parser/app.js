c852233fd92dae38ddfefb8aeb716e66cb20a252
Dk22121994 <119493585+Dk22121994@users.noreply.github.com>
2023-09-19 15:36:49 +0530

    Add files via upload

--- ccda-node/ccda-parser/app.js
+++ ccda-node/ccda-parser/app.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const bb = require('@amida-tech/blue-button');
 
+// Read the CCDA document from a file
+const ccdaXml = fs.readFileSync('data/input/ccda_sample1.xml', 'utf-8');
+
+// Parse the CCDA document
+const parsedCcda = bb.parse(ccdaXml);
+
+// Access patient information
+const patient = parsedCcda.data.demographics;
+// Access medication information
+const medications = parsedCcda.data.medications;
+// Access problem information
+const problems = parsedCcda.data.problems;
+
+console.log('Patient:', patient);
+console.log('Medications:', medications);
+console.log('Problems:', problems);
