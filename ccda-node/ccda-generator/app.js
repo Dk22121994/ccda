0086d2fcb42ac4a6b9685ed502c04ae3a65e1163
Dk22121994 <119493585+Dk22121994@users.noreply.github.com>
2023-09-20 11:02:06 +0530

    Add files via upload

--- ccda-node/ccda-generator/app.js
+++ ccda-node/ccda-generator/app.js
@@ -1,1 +1,6 @@
-
+const fs = require('fs');
+var bbg = require("@amida-tech/blue-button-generate")
+var doc = fs.readFileSync('./data/input/sample_ccda_input.json', 'utf-8');
+const docObj = JSON.parse(doc);
+var modifiedDataCCD = bbg.generateCCD(docObj);
+fs.writeFileSync('./data/output/generated_ccda.xml', modifiedDataCCD);
